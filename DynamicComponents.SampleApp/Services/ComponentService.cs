4ad3897618efd74f4ad6ab82198fcb5d1137e3d9
Fabio <spaziani.fa@gmail.com>
2022-09-06 14:46:38 +0200

    Aggiungere i file di progetto.

---
+++ DynamicComponents.SampleApp/Services/ComponentService.cs
@@ -0,0 +1,37 @@
+﻿using DynamicComponents.SampleApp.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DynamicComponents.SampleApp.Services
+{
+    public class ComponentService
+    {
+        private readonly IJSRuntime iJSRuntime;
+
+        public ComponentService(IJSRuntime iJSRuntime)
+        {
+            this.iJSRuntime = iJSRuntime;
+        }
+
+        public Dictionary<string, RenderComponent> GetRenderComponents()
+        {
+            return new Dictionary<string, RenderComponent>()
+            {
+                {"Posta ordinaria", new RenderComponent() { Type = typeof(AddressComponent) } },
+
+                {"Fax", new RenderComponent() {
+                    Type = typeof(FaxComponent)
+                    , Parameters = new(){
+                        { "CountryCode" , "+39"} ,
+                        { "OnExportButtonClick", EventCallback.Factory.Create<string>(this, OnExportClick) }
+                    }
+                }}
+            };
+        }
+
+        private async Task OnExportClick(string faxNumberValue)
+        {
+            await iJSRuntime.InvokeVoidAsync("alert", faxNumberValue);
+        }
+    }
+}
---
+++ DynamicComponents.SampleApp/Services/RenderComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace DynamicComponents.SampleApp.Services
+{
+    public class RenderComponent
+    {
+        public Type Type { get; set; }
+        public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
+    }
+}
